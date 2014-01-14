784eb4884cafac34ece1264f61e532e9ce1df755
Baptiste Meurant <baptiste.meurant@atos.net>
2014-01-14 22:32:26 +0100

    css refactor by @loicfrering

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,4 +5,12 @@ $(function () {
         $('.search-toggle').toggleClass('active');
     });
 
+    $('input[type=search]').on('focus', function(){
+        // replace CSS font-size with 16px to disable auto zoom on iOS
+        $(this).data('fontSize', $(this).css('font-size')).css('font-size', '20px');
+    }).on('blur', function(){
+            // put back the CSS font-size
+            $(this).css('font-size', $(this).data('fontSize'));
+        });
+
 });
