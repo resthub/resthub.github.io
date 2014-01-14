50fc84555e7a67cdce01b2fd929e12185a014f8f
Baptiste Meurant <baptiste.meurant@atos.net>
2014-01-14 13:46:46 +0100

    customize responsive

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    $('.navbar-nav .fa-search').on('click', function () {
+    $('.navbar-nav.visible-xs .fa-search').on('click', function () {
         $('.search-dropdown-container').toggleClass('hidden');
         $('.search-toggle').toggleClass('active');
     });
