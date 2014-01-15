8b246449eeb2a394d8857cac97dac50b32e3ecc8
Baptiste Meurant <baptiste.meurant@atos.net>
2014-01-15 03:05:03 +0100

    css refactor for navbar

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,16 +1,8 @@
 $(function () {
 
-    $('.navbar-nav.visible-xs .fa-search').on('click', function () {
-        $('.search-dropdown-container').toggleClass('hidden');
+    $('.navbar-search').on('click', function () {
+        $('.search-form').toggleClass('hidden');
         $('.search-toggle').toggleClass('active');
     });
-
-    $('input[type=search]').on('focus', function(){
-        // replace CSS font-size with 16px to disable auto zoom on iOS
-        $(this).data('fontSize', $(this).css('font-size')).css('font-size', '20px');
-    }).on('blur', function(){
-            // put back the CSS font-size
-            $(this).css('font-size', $(this).data('fontSize'));
-        });
 
 });
