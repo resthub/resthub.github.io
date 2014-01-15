c8fdada8631e34abf30aa36133967a38e471bce8
Baptiste Meurant <baptiste.meurant@atos.net>
2014-01-15 16:26:06 +0100

    add navigation links for xs and xxs*

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,4 +5,9 @@ $(function () {
         $('.search-toggle').toggleClass('active');
     });
 
+    $('.collapsible-menu .toggle-menu').on('click', function () {
+        $('.collapsible-menu .menu').toggleClass('hidden');
+        $('.collapsible-menu .toggle-menu').toggleClass('open');
+    });
+
 });
